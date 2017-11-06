1732e3354cafd449ce1b000e12f1cca5881ebd46
Mylife4air <1071484952@qq.com>
2017-11-06 17:50:05 +0800

    Update pricesensor.py

--- components/pricesensor.py
+++ components/pricesensor.py
@@ -5,16 +5,79 @@
 """
 import logging
 from homeassistant.helpers.entity import Entity
+# 引入这两个库，用于配置文件格式校验
+import voluptuous as vol
+import homeassistant.helpers.config_validation as cv
 _LOGGER = logging.getLogger(__name__)
-
+DOMAIN = 'pricesensor'
+URL = "url"
+DEFAULT_URL="https://item.taobao.com/item.htm?scm=1007.12493.92624.100200300000005&id=549816031459&pvid=ccf56d0c-3737-4$
+# 配置文件的样式
+CONFIG_SCHEMA = vol.Schema(
+    {
+        DOMAIN: vol.Schema(
+            {
+                vol.Required(URL,default = DEFAULT_URL):cv.string,
+            }),
+    },
+    extra=vol.ALLOW_EXTRA)
 def setup_platform(hass, config, add_devices, discovery_info=None):   #在homeassistant注册实体
     _LOGGER.info("setup platform sensor.hachina...")
-    add_devices([HAChinaPriceSensor()])
+    add_devices([HAChinaPriceSensor(config)])
 
-class HAChinaPriceSensor(Entity):  
-    def __init__(self):          #初始化传感器实体
+class HAChinaPriceSensor(Entity):
+    def __init__(self,config):          #初始化传感器实体
         self._object_id = 'pricesensor'
         self._state = 0
+"""
+文件名：pricesensor.py
+文件位置：HomeAssistant配置目录/custom_components/sensor/pricesensor.py
+
+"""
+import logging
+from homeassistant.helpers.entity import Entity
+# 引入这两个库，用于配置文件格式校验
+import voluptuous as vol
+import homeassistant.helpers.config_validation as cv
+_LOGGER = logging.getLogger(__name__)
+DOMAIN = 'pricesensor'
+URL = "url"
+DEFAULT_URL="https://item.taobao.com/item.htm?scm=1007.12493.92624.100200300000005&id=549816031459&pvid=ccf56d0c-3737-4$
+# 配置文件的样式
+CONFIG_SCHEMA = vol.Schema(
+    {
+        DOMAIN: vol.Schema(
+            {
+                vol.Required(URL,default = DEFAULT_URL):cv.string,
+            }),
+    },
+    extra=vol.ALLOW_EXTRA)
+def setup_platform(hass, config, add_devices, discovery_info=None):   #在homeassistant注册实体
+    _LOGGER.info("setup platform sensor.hachina...")
+    add_devices([HAChinaPriceSensor(config)])
+
+class HAChinaPriceSensor(Entity):
+    def __init__(self,config):          #初始化传感器实体
+        self._object_id = 'pricesensor'
+        self._state = 0
+
+    
+    {
+        DOMAIN: vol.Schema(
+            {
+                vol.Required(URL,default = DEFAULT_URL):cv.string,
+            }),
+    },
+    extra=vol.ALLOW_EXTRA)
+def setup_platform(hass, config, add_devices, discovery_info=None):   #在homeassistant注册实体
+    _LOGGER.info("setup platform sensor.hachina...")
+    add_devices([HAChinaPriceSensor(config)])
+
+class HAChinaPriceSensor(Entity):
+    def __init__(self,config):          #初始化传感器实体
+        self._object_id = 'pricesensor'
+        self._state = 0
+        self._url = config['url']
         self._unit_of_measurement='￥'
     @property
     def unit_of_measurement(self):       #用于呈现传感器符号
@@ -27,10 +90,10 @@ class HAChinaPriceSensor(Entity):
         return self._state
     def update(self):               #更新传感器状态
         import requests
-        from lxml import etree
-        url ='https://item.taobao.com/item.htm?scm=1007.12493.92624.100200300000005&id=549816031459&pvid=ccf56d0c-3737-4863-ab35-206b76d014d3'
-        req= requests.get(url)
-        tree = etree.HTML(req.text)
-        data = tree.xpath('//*[@id="J_StrPrice"]/em[2]/text()')
-        self._state = str(data[0])
-    
+        import re
+        req = requests.get('self._url')
+        data = re.findall('"tm-count">(.*?)</span>',req.text,re.S) 
+        self._state=str(re.findall('\d+',str(data))[0])
+
+        
+
