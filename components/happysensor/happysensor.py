2808bc409d44f93811e1cb483839f22b3a91afdd
Mylife4air <1071484952@qq.com>
2017-11-15 10:44:36 +0800

    Update happysensor.py

--- components/happysensor/happysensor.py
+++ components/happysensor/happysensor.py
@@ -2,8 +2,6 @@
 文件名：happysensor.py
 文件位置：HomeAssistant配置目录/custom_components/sensor/happysensor.py
 """
-import asyncio
-from homeassistant.helpers.aiohttp_client import async_get_clientsession
 import logging
 from homeassistant.helpers.entity import Entity
 # 引入这两个库，用于配置文件格式校验
@@ -22,31 +20,35 @@ CONFIG_SCHEMA = vol.Schema(
     },
     extra=vol.ALLOW_EXTRA)
 
-def setup_platform(hass, config, add_devices, discovery_info=None):   #加载组件
+def setup_platform(hass, config, add_devices, discovery_info=None):   #加载传感器实体
     _LOGGER.info("setup platform sensor.hachina...")
     add_devices([HAChinaPriceSensor(hass,config)])
 
 
 class HAChinaPriceSensor(Entity):
-    def __init__(self,hass,config):          #初始化笑话传感器实体
+    def __init__(self,hass,config):          #初始化传感器实体
         self._object_id = 'happysensor'
         self._state = "请稍等一下"
         self._hass = hass
         self._apk = config['appkey']
         self._url = URL+self._apk
+        hass.service.register(DOMAIN,'happy_update',self.device_update)
 
     @property
-    def name(self):                       #用于呈现笑话传感器名字
+    def name(self):                       #用于呈现传感器名字
         return self._object_id
+
     @property
-    def state(self):                   #用于呈现笑话传感器状态
+    def state(self):                   #用于呈现传感器状态
         return self._state
 
-    @asyncio.coroutine
-    def async_update(self):               #异步更新笑话传感器状态
-        import json
-        session = async_get_clientsession(self._hass)
-        resp = yield from session.get(self._url)
-        result = yield from resp.text()
+
+    def device_update(self):               #更新传感器状态
+        import requests,json
+        result=requests.get(self._url).text
         data=json.loads(result, strict=False)['result']['list'][0]['content']
         self._state=data
+
+    def update(self):
+        pass
+
