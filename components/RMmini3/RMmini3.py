b3f3dc079d70c53d66e85d358769783f3dc4b079
Mylife4air <1071484952@qq.com>
2017-11-22 16:25:45 +0800

    Update RMmini3.py

--- components/RMmini3/RMmini3.py
+++ components/RMmini3/RMmini3.py
@@ -5,44 +5,46 @@ from homeassistant.helpers.entity import Entity
 import voluptuous as vol
 import homeassistant.helpers.config_validation as cv
 import broadlink
-import  os
+import os
 import time
 DOMAIN = 'RMmini3'
-type = 0x2737
+type1 = 0x2737
 path = os.path.expanduser('~')+'/.homeassistant/learn'
 # 配置文件的样式
 CONFIG_SCHEMA = vol.Schema(
     {
         DOMAIN: vol.Schema(
             {
-                vol.Required('mac'):cv.string,
-                vol.Required('host'):cv.port,
+                vol.Required('mac'): cv.string,
+                vol.Required('host'): cv.port,
             }),
     },
     extra=vol.ALLOW_EXTRA)
 
 
-def setup_platform(hass, config, add_devices, discovery_info=None):        #装载实体
-    """Set up the Demo sensors."""
+def setup_platform(hass, config, add_devices):
+    """装载实体"""
     host = config['host']
-    mac=bytearray.fromhex(config['mac'])
-    dev = broadlink.gendevice(type, (host, 80), mac)
-    o =Rmmin3(hass,dev)
+    mac = bytearray.fromhex(config['mac'])
+    dev = broadlink.gendevice(type1, (host, 80), mac)
+    o = Rmmin3(hass, dev)
     add_devices([o])
 
 
 class Rmmin3(Entity):
     """创建RMmini3类."""
 
-    def __init__(self,hass,dev ):
-        """初始化."""
+    def __init__(self, ha, dev):
+        """初始化并注册学习服务和发送服务"""
         self._state = '未知'
-        self._hass = hass
+        self._ha = ha
         self._dev = dev
         self._name = DOMAIN
+        self._message = ''
         self._unit_of_measurement = '度'
-        hass.services.register(DOMAIN,'send',self.send)   #注册学习服务和发送服务
-        hass.services.register(DOMAIN, 'learn', self.learn)
+        self._hass.services.register(DOMAIN, 'send', self.send)
+        self._hass.services.register(DOMAIN, 'learn', self.learn)
+
     @property
     def should_poll(self):
         """需要更新温度"""
@@ -68,7 +70,8 @@ class Rmmin3(Entity):
         self._dev.auth()
         self._state = self._dev.check_temperature()
 
-    def send(self, data):   #发送红外信息
+    def send(self, data):
+        """发送红外信息"""
         self._dev.auth()
         with open(path, 'r') as fs:
             for line in fs:
@@ -78,7 +81,8 @@ class Rmmin3(Entity):
                     self._dev.send_data(date)
                     break
 
-    def learn(self, date):          #学习红外信息
+    def learn(self, date):
+        """学习红外信息"""
         self._dev.auth()
         self._dev.enter_learning()
         data = None
@@ -89,8 +93,8 @@ class Rmmin3(Entity):
             data = self._dev.check_data()
         if data:
             learned = ''.join(format(x, '02x') for x in bytearray(data))
-            self._message= learned
-            with open(path,'a+') as fs:
+            self._message = learned
+            with open(path, 'a+') as fs:
                 fs.write(str(date.data['learn']))
                 fs.write(':')
                 fs.write(str(learned))
@@ -99,4 +103,3 @@ class Rmmin3(Entity):
                 fs.close()
         else:
             print("No data received...")
-
